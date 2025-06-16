79905dc2045aaf111e88ede7d876a2c8fc72ae80
Bohdan Leshchenko <bohdan.leshchenko1@gmail.com>
2025-06-16 22:08:36 +0200

    upd code

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -7,10 +7,32 @@ import (
 
 type Cache struct {
 	Entry map[string]cacheEntry
-	m     sync.Mutex
+	mu    sync.RWMutex
 }
 
 type cacheEntry struct {
-	CreateAt time.Time
-	Value    []byte
+	CreatedAt time.Time
+	Value     []byte
 }
+
+func NewCache(interval time.Duration) *Cache {
+
+}
+
+func (c *Cache) Add(key string, value []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	entry := cacheEntry{CreatedAt: time.Now(), Value: value}
+	c.Entry[key] = entry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	data, ok := c.Entry[key]
+	return data.Value, ok
+}
+
+func (c *Cache) reapLoop() {}
